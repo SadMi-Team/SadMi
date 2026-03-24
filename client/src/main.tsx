bec1ab08a1c5cced5dad28ae3f3c8be34dc3fd36
0gudu <gugag3rr@gmail.com>
2026-03-24 16:39:06 -0300

    :wrench: chore: Initial project setup

---
+++ client/src/main.tsx
@@ -0,0 +1,22 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { Provider } from "@/components/ui/provider";
+
+import "@/index.css";
+import App from "@/App.tsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <Provider>
+      <RouterProvider router={router} />
+    </Provider>
+  </StrictMode>,
+);
---
+++ shared/index.ts
@@ -0,0 +1,3 @@
+export const saudacao = (nome: string) => {
+  return `Olá ${nome}, isso veio da pasta shared!`;
+};
