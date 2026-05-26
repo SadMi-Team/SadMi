124a566a5c931da443a2dbff9a05641f8eef2dc4
0gudu <gugag3rr@gmail.com>
2026-05-26 01:55:32 -0300

    :sparkles: feat: criada listagem dos clientes e finalizado estilizaçõ da tela

--- client/src/Login.tsx
+++ client/src/Login.tsx
@@ -2,24 +2,24 @@ import { Flex, Box, Button, Field, Input, Image } from "@chakra-ui/react";
 import { PasswordInput } from "@/components/ui/password-input";
 import { Toaster, toaster } from "@/components/ui/toaster";
 import { useMutation } from "@tanstack/react-query";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { useNavigate } from "react-router";
 
-import axios from "axios";
+import api from "@/utils/axios";
 
 interface ApiError {
- response?: {
+  response?: {
     data?: ApiReturn;
- };
+  };
 }
 
 interface ApiReturn {
- erro?: string | ApiReturnMessage[];
- error?: string;
+  erro?: string | ApiReturnMessage[];
+  error?: string;
 }
 
 interface ApiReturnMessage {
- message: string;
+  message: string;
 }
 
 interface LoginData {
@@ -33,9 +33,11 @@ interface LoginData {
 }
 
 const loginRequest = (data: LoginData) =>
-  axios.post(import.meta.env.VITE_API_URL + "/auth/login", data, {
-    withCredentials: true,
-  });
+  api
+    .post(import.meta.env.VITE_API_URL + "/auth/login", data, {
+      withCredentials: true,
+    })
+    .then((res) => res.data);
 
 function App() {
   const [username, setUsername] = useState("");
@@ -47,15 +49,23 @@ function App() {
 
   const login = useMutation({
     mutationFn: loginRequest,
-    onError: (error : ApiError) => {
+    onSuccess: (success) => {
+      console.log(success);
+      if (success.usuario.perfil == "administrador") {
+        navigate("/admin");
+      } else {
+        navigate("/cliente");
+      }
+    },
+    onError: (error: ApiError) => {
       const data = error.response?.data;
 
-      let msgTitle = "Erro"; 
+      let msgTitle = "Erro";
 
       if (typeof data?.erro === "string") {
-        msgTitle = data.erro; 
+        msgTitle = data.erro;
       } else if (Array.isArray(data?.erro) && data.erro.length > 0) {
-        msgTitle = data.erro[0].message; 
+        msgTitle = data.erro[0].message;
       }
       const msgDesc = data?.error || "Descrição desconhecida";
 
@@ -85,7 +95,7 @@ function App() {
       return;
     }
 
-    const data : LoginData = {
+    const data: LoginData = {
       email: username,
       senha: password,
     };
@@ -93,25 +103,8 @@ function App() {
     login.mutate(data);
   };
 
-  useEffect(() => {
-    if (login.data) {
-      if(login.data.data.usuario.perfil == "administrador") {
-        navigate("/admin");
-      }else {
-        navigate("/cliente");
-      }
-      
-    }
-  }, [login.data, navigate]);
-
   return (
-    <Flex
-      w="100%"
-      h="95vh"
-      justify="center"
-      align="center"
-      direction="column"
-    >
+    <Flex w="100%" h="95vh" justify="center" align="center" direction="column">
       <Toaster />
       <Box>
         <Image rounded="md" src="sadmi-logo.png" alt="John Doe" margin="8" />
@@ -138,7 +131,9 @@ function App() {
             <Field.Label>Email</Field.Label>
             <Input
               value={username}
-              onChange={(e : { target: { value : string}}) => setUsername(e.target.value)}
+              onChange={(e: { target: { value: string } }) =>
+                setUsername(e.target.value)
+              }
               placeholder="Coloque seu e-mail"
             />
             <Field.ErrorText>Favor preencher o campo de email</Field.ErrorText>
@@ -148,7 +143,9 @@ function App() {
             <PasswordInput
               placeholder="Coloque sua senha"
               value={password}
-              onChange={(e : { target: { value : string}}) => setPassword(e.target.value)}
+              onChange={(e: { target: { value: string } }) =>
+                setPassword(e.target.value)
+              }
             />
             <Field.ErrorText>Favor preencher o campo de senha</Field.ErrorText>
           </Field.Root>
