58efbf2a097cccb30c6c932ede42a7756db7494e
GUSTAVO JOSÉ PAULO <gustavo.paulo@catolicasc.edu.br>
2026-06-02 20:19:25 -0300

    :lipstick: feat: Adicionado componentes na tela

--- client/src/Cliente.tsx
+++ client/src/Cliente.tsx
@@ -1,8 +1,24 @@
-import { Flex, Image, Text, Button } from "@chakra-ui/react";
-import { LuUsers, LuLogOut } from "react-icons/lu";
+import {
+  Flex,
+  Image,
+  Text,
+  Button,
+  Card,
+  SimpleGrid,
+  Table,
+  Status,
+} from "@chakra-ui/react";
+import {
+  LuLogOut,
+  LuActivity,
+  LuCircleCheck,
+  LuPackage,
+  LuTriangleAlert,
+} from "react-icons/lu";
 import { useMutation } from "@tanstack/react-query";
 import { useNavigate } from "react-router";
 import { Toaster, toaster } from "@/components/ui/toaster";
+import { ColorModeButton } from "@/components/ui/color-mode";
 
 import api from "./utils/axios";
 import CCard from "./components/CCard";
@@ -28,6 +44,10 @@ interface ApiReturn {
 
 interface ApiReturnMessage {
   message: string;
+}
+
+interface MaquinaCard {
+  nome: string;
 }
 
 const logoutRequest = () =>
@@ -67,38 +87,44 @@ function App() {
   });
   const cards: CCards[] = [
     {
-      color: "red",
-      title: "Taxa de Utilização",
-      icon: <LuUsers />,
-      value: "87.3%",
-      subtitle: "Média global",
-    },
-    {
       color: "blue",
-      title: "Total de Clientes",
-      icon: <LuUsers />,
-      value: "0",
-      subtitle: "0",
+      title: "OEE Médio",
+      icon: <LuActivity />,
+      value: "87.0%",
+      subtitle: "Eficiência Global",
     },
     {
       color: "green",
-      title: "Maquinas Cadastradas",
-      icon: <LuUsers />,
-      value: "26",
-      subtitle: "Em todos os clientes",
+      title: "Máquinas Ativas",
+      icon: <LuCircleCheck />,
+      value: "5/8",
+      subtitle: "Em operação",
     },
     {
-      color: "red",
-      title: "Taxa de Utilização",
-      icon: <LuUsers />,
-      value: "87.3%",
-      subtitle: "Média global",
+      color: "purple",
+      title: "Produção Semanal",
+      icon: <LuPackage />,
+      value: "7,560",
+      subtitle: "Peças produzidas",
+    },
+    {
+      color: "yellow",
+      title: "Alertas",
+      icon: <LuTriangleAlert />,
+      value: "2",
+      subtitle: "Requerem atenção",
     },
   ];
 
   return (
-    <Flex w="100%" h="full" direction="column" justify="center" align="center">
-      {" "}
+    <Flex
+      w="100%"
+      h="full"
+      direction="column"
+      justify="center"
+      align="center"
+      gap="2"
+    >
       <Flex
         w="100%"
         h="20"
@@ -134,14 +160,17 @@ function App() {
             </Flex>
           </Flex>
 
-          <Button
-            colorPalette="gray"
-            variant="surface"
-            onClick={() => logout.mutate()}
-          >
-            <LuLogOut />
-            <Text>Sair</Text>
-          </Button>
+          <Flex align="center" gap="2">
+            <ColorModeButton />
+            <Button
+              colorPalette="gray"
+              variant="surface"
+              onClick={() => logout.mutate()}
+            >
+              <LuLogOut />
+              <Text>Sair</Text>
+            </Button>
+          </Flex>
         </Flex>
       </Flex>
       <Flex
@@ -156,7 +185,129 @@ function App() {
           <CCard {...card} key={index} />
         ))}
       </Flex>
+      <Flex
+        w={{ base: "90%", md: "80%" }}
+        justify="space-between"
+        direction={{ base: "column", md: "row" }}
+        gap="2"
+      >
+        <Card.Root w={{ base: "100%", md: "49%" }} shadow="md">
+          <Card.Header padding="0">
+            <Flex
+              w="100%"
+              justify="space-between"
+              padding="2"
+              direction="column"
+            >
+              <Text color="fg.info" fontWeight="semibold">
+                Gerenciar Clientes
+              </Text>
+              <Text color="fg.muted" textStyle="xs">
+                Cadastre e gerencie as contas de clientes do sistema
+              </Text>
+            </Flex>
+          </Card.Header>
+          <Card.Body color="fg.muted">
+            This is the card body. Lorem ipsum dolor sit amet, consectetur
+            adipiscing elit.
+          </Card.Body>
+        </Card.Root>
+        <Card.Root w={{ base: "100%", md: "49%" }} shadow="md">
+          <Card.Header padding="0">
+            <Flex
+              w="100%"
+              justify="space-between"
+              padding="2"
+              direction="column"
+            >
+              <Text color="fg.info" fontWeight="semibold">
+                Gerenciar Clientes
+              </Text>
+              <Text color="fg.muted" textStyle="xs">
+                Cadastre e gerencie as contas de clientes do sistema
+              </Text>
+            </Flex>
+          </Card.Header>
+          <Card.Body color="fg.muted">
+            This is the card body. Lorem ipsum dolor sit amet, consectetur
+            adipiscing elit.
+          </Card.Body>
+        </Card.Root>
+      </Flex>
+      <Card.Root w={{ base: "90%", md: "80%" }} shadow="md">
+        <Card.Header padding="0">
+          <Flex w="100%" justify="space-between" padding="2">
+            <Flex direction="column">
+              <Text color="fg.info" fontWeight="semibold">
+                Gerenciar Clientes
+              </Text>
+              <Text color="fg.muted" textStyle="xs">
+                Cadastre e gerencie as contas de clientes do sistema
+              </Text>
+            </Flex>
+            <Button colorPalette="blue">+ Nova Maquina</Button>
+          </Flex>
+        </Card.Header>
+        <Card.Body>
+          <SimpleGrid columns={{ base: 1, sm: 2, md: 3, lg: 4 }} gap="2">
+            <Maquina nome="anos" />
+            <Maquina nome="meses" />
+            <Maquina nome="dias" />
+            <Maquina nome="horas" />
+          </SimpleGrid>
+        </Card.Body>
+      </Card.Root>
     </Flex>
   );
 }
 export default App;
+
+function Maquina({ nome }: MaquinaCard) {
+  return (
+    <Card.Root size="sm">
+      <Card.Header>
+        <Flex justify="space-between">
+          <Text fontWeight="semibold">{nome}</Text>
+          <Status.Root colorPalette="red">
+            <Status.Indicator />
+          </Status.Root>
+        </Flex>
+      </Card.Header>
+      <Card.Body color="fg.muted">
+        <Table.Root size="sm">
+          <Table.Header></Table.Header>
+          <Table.Body>
+            <Table.Row>
+              <Table.Cell>Status:</Table.Cell>
+              <Table.Cell>
+                <Flex justify="end" w="100%">
+                  <Text
+                    bg="blue.fg"
+                    w="fit-content"
+                    rounded="md"
+                    paddingLeft="1"
+                    paddingRight="1"
+                    color="fg.inverted"
+                  >
+                    Em Operação
+                  </Text>
+                </Flex>
+              </Table.Cell>
+            </Table.Row>
+            <Table.Row>
+              <Table.Cell>OEE:</Table.Cell>
+              <Table.Cell>
+                <Flex justify="end" w="100%">
+                  <Text color="fg.info">2</Text>
+                </Flex>
+              </Table.Cell>
+            </Table.Row>
+            <Table.Row>
+              <Table.Cell>1</Table.Cell>
+            </Table.Row>
+          </Table.Body>
+        </Table.Root>
+      </Card.Body>
+    </Card.Root>
+  );
+}
